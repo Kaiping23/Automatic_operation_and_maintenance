319a646a96749deee69e49b0d7ac2a134ea3cf62
Kaiping23 <990814268@qq.com>
2019-12-17 18:47:16 +0800

    send amn e-mail to your heart

--- everyday.log.sql
+++ everyday.log.sql
@@ -32,8 +32,12 @@
 https://raw.githubusercontent.com/a001189/scripts/master/init_intall.sh
 
 -- 新加服务器，需要在新的服务器上完成以下操作
+    购买相同配置服务器选择 包年包月 华东2 可用区B 自定义镜像 16c 64G 存储 系统盘 SSD云盘100G 数据盘高效云盘 400G 专有网络wmy-prod 交换机wmy-prod-vs
+    分配公网ip 带宽计费模式 按流量使用 带宽峰值 100M 安全组 弹性网卡wmy-prod-vs 实例名称 主机名
+    app-add-images-base 加入到默认资源组 专用网络
     oss挂载
-    eureka添加服务
+    添加服务eureka
+
 
 sit环境cleverbee服务域名:
 sit.authzmenu.kmelearning.com
@@ -43,6 +47,13 @@ sudo systemctl enable sshd
 
 sudo systemctl start sshd
 
+yum groupinstall "X Window System" -y
+# openoffice4解析失败   安装文泉驿中文字体
+yum install -y 'wqy-*'
+
 220.248.15.46
+
+193035 改类型为微系列
+
 
 
